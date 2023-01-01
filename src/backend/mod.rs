8f5331b3b58248337158b10cc8bbe14f2032e447
Maximiliano Sandoval R <msandova@gnome.org>
2023-01-01 16:58:14 +0100

    backend: Add FileChooser portal support

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,9 +1,14 @@
 // mod access;
 // mod account;
+mod file_chooser;
 // mod request;
 // mod session;
 mod wallpaper;
 
+pub use file_chooser::{
+    FileChooser, FileChooserImpl, OpenFileOptions, OpenFileResults, SaveFileOptions,
+    SaveFileResults, SaveFilesOptions, SaveFilesResults,
+};
 pub use wallpaper::{Wallpaper, WallpaperImpl, WallpaperOptions};
 
 pub(crate) const IMPL_PATH: &str = "/org/freedesktop/portal/desktop";
